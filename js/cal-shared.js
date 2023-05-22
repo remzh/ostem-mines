f5ff8f557d8910be70446132507f7e3369ed7677
Rem Zhang <hello@itsrem.org>
2023-05-21 18:05:37 -0600

    better calendar button, better anchors

--- js/cal-shared.js
+++ js/cal-shared.js
@@ -1,3 +1,9 @@
+import { atcb_action } from './add-to-calendar.js';
+
+function formatDesc(input) {
+  return input.replace(/<a.*?>/g, '[url]').replace(/<\/a>/g, '[/url]').replace(/<.+?>/g, s => `{${s.slice(1, s.length-1)}}`) + `[br][br][i](This event does not sync with the official oSTEM@Mines calendar. Visit the [url]https://go.itsrem.org/ostem/hub|oSTEM Hub[/url] for the latest information.)[/i]`;
+}
+
 export function formatDate(start, end, allDay, shortForm=false) {
   console.log(start, end);
   if (allDay) {
@@ -11,24 +17,27 @@ export function formatDate(start, end, allDay, shortForm=false) {
 export function showEvent(event) {
   import('/js/popup.js')
     .then((module) => {
-      module.create(`<h1>${event.title}</h1><p><span class='p2'>${formatDate(event.start, event.end, event.allDay)}</span></p><hr/><p id='_tmp_cal_eventDesc'></p><hr/><div><button id='_rem-popup-dismiss' class='right'>Done</button></div>`);
-      let calEle = document.createElement('add-to-calendar-button');
-      calEle.setAttribute('inline', true);
-      calEle.setAttribute('timeZone', 'America/Denver');
-      calEle.setAttribute('location', event.loc ? event.loc : 'Not Specified')
-      calEle.setAttribute('name', event.title);
-      calEle.setAttribute('label', 'Add Event to Calendar');
-      calEle.setAttribute('description', event.desc);
-      calEle.setAttribute('trigger', 'click');
-      calEle.setAttribute('startDate', event.start.toISOString().substring(0, 10));
-      calEle.setAttribute('options', JSON.stringify(['Google|Google Calendar', 'Microsoft365|Outlook Calendar', 'Apple|Apple Calendar', 'iCal|iCal File']));
+      module.create(`<h1>${event.title}</h1><p><span class='p2'>${formatDate(event.start, event.end, event.allDay)}</span></p><hr/><p id='_tmp_cal_eventDesc'></p><div><button id='_rem-popup-dismiss' class='right'>Done</button><button id='_rem-popup-addToCal' class='_rem-popup-action'><span class='material-symbols-rounded' style='vertical-align: text-top'>calendar_add_on</span> Add Event to Calendar</button></div>`);
+
+      let calConfig = {
+        timeZone: 'America/Denver',
+        name: event.title,
+        description: formatDesc(event.desc),
+        startDate: event.start.toISOString().substring(0, 10),
+        options: ['Google|Google Calendar', 'Microsoft365|Outlook Calendar', 'Apple|Apple Calendar', 'iCal|iCal File']
+      };
+
       if (event.allDay) {
-        calEle.setAttribute('endDate', new Date(event.end - 1).toISOString().substring(0, 10));
+        calConfig.endDate = new Date(event.end - 1).toISOString().substring(0, 10);
       } else {
-        calEle.setAttribute('startTime', `${event.start.getHours().toString().padStart(2, '0')}:${event.start.getMinutes().toString().padStart(2, '0')}`);
-        calEle.setAttribute('endTime', `${event.end.getHours().toString().padStart(2, '0')}:${event.end.getMinutes().toString().padStart(2, '0')}`);
+        calConfig.startTime = `${event.start.getHours().toString().padStart(2, '0')}:${event.start.getMinutes().toString().padStart(2, '0')}`;
+        calConfig.endTime = `${event.end.getHours().toString().padStart(2, '0')}:${event.end.getMinutes().toString().padStart(2, '0')}`;
       }
-      document.getElementById('_rem-popup-dismiss').insertAdjacentElement('beforebegin', calEle);
+
+      let calBtn = document.getElementById('_rem-popup-addToCal');
+      calBtn.addEventListener('click', () => atcb_action(calConfig, calBtn));
+
+      // document.getElementById('_rem-popup-dismiss').insertAdjacentElement('beforebegin', calEle);
       document.getElementById('_tmp_cal_eventDesc').innerHTML = event.desc ? event.desc : `<i>No description provided.</i>`;
       document.getElementById('_tmp_cal_eventDesc').removeAttribute('id');
     })
